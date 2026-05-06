fd1af412a622a9037f182af4e206a79c2e56b52b
er-sahilbharti <sahil.884062@gmail.com>
2026-05-06 16:30:36 +0530

    login expired/invalid token

--- src/api/axios.js
+++ src/api/axios.js
@@ -22,4 +22,16 @@ api.interceptors.request.use(
   }
 );
 
+// Response interceptor to handle 401 Unauthorized (expired/invalid token)
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
